8a83cae41fffd573436555d8030ca8b638166e02
ani <godwin@istrategytech.com>
2023-06-06 14:58:37 +0100

    play video

--- app/src/main/java/com/example/anisvideoplayerapp/MainActivity.kt
+++ app/src/main/java/com/example/anisvideoplayerapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.anisvideoplayerapp
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.MediaController
@@ -14,5 +15,18 @@ class MainActivity : AppCompatActivity() {
         videoView.setVideoPath("android.resource://"
                                 +packageName+"/"+R.raw.aggallio)
         val mediaController  = MediaController(this@MainActivity)
+        mediaController.setAnchorView(videoView)
+        mediaController.setMediaPlayer(videoView)
+        videoView.setMediaController(mediaController)
+
+        val secondVideoView : VideoView = findViewById(R.id.secondVideoView)
+        val mediaController2  = MediaController(this@MainActivity)
+
+        var uri : Uri = Uri.parse("https://youtu.be/dVtxHoSWlws")
+        secondVideoView.setVideoURI(uri)
+        mediaController2.setAnchorView(secondVideoView)
+        mediaController2.setMediaPlayer(secondVideoView)
+        secondVideoView.setMediaController(mediaController2)
+
     }
 }
